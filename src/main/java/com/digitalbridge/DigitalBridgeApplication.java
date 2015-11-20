df6cb5a5c422c2913f427f1144d4f923ae697b60
rajadileepkolli <rajadileepkolli@users.noreply.github.com>
2015-11-20 16:15:49 +0530

    Initial version of Project

---
+++ src/main/java/com/digitalbridge/DigitalBridgeApplication.java
@@ -0,0 +1,12 @@
+package com.digitalbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBridgeApplication.class, args);
+    }
+}
